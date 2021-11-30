8864ed4fa34eeaa1f6708339da83825f3397fe6b
Bonge <35810849+bonge-ian@users.noreply.github.com>
2021-11-30 21:09:14 +0300

    Added seeders for Role and User Resource

---
+++ database/seeders/RoleUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class RoleUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::where('name', 'user')->first();
+
+        User::all()->each(
+            fn ($user) => $user->roles()->attach($role)
+        );
+    }
+}
